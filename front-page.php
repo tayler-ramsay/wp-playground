c6032d6d3c3159c43095ca763db8cd86a0dacbe8
Tayler Ramsay <ramsay.tayler@gmail.com>
2017-07-26 15:16:28 -0400

    setup front page

---
+++ front-page.php
@@ -0,0 +1,28 @@
+<?php
+
+get_header(); ?>
+
+
+<!--site-content-->
+<div class="site-content clearfix">
+    <?php if (have_posts()) :
+      while (have_posts()) : the_post();
+
+    the_content();
+
+    endwhile;
+
+    else :
+      echo '<p>No content found</p>';
+
+    endif; ?>
+
+    
+
+</div>
+<!--site-content-->
+
+
+<?php get_footer();
+
+ ?>
