bf20da87faa54daf33e933a62abb7784c85bae12
Tayler Ramsay <ramsay.tayler@gmail.com>
2017-07-28 12:20:54 -0400

    frontpage

--- front-page.php
+++ front-page.php
@@ -1,28 +1,58 @@
-<?php
-
-get_header(); ?>
+<?php get_header(); ?>
 
 
 <!--site-content-->
 <div class="site-content clearfix">
-    <?php if (have_posts()) :
-      while (have_posts()) : the_post();
+    <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; ?>
+    <?php else: '<p>No content found</p>' ?>
+    <?php endif; ?>
+<div class="home-columns clearfix">
+<div class="one-half-column">
+<!-- random posts loop begins here -->
+<?php $randomPosts = new WP_Query('cat=5&posts_per_page=2&orderby=ran'); ?>
 
-    the_content();
+  <?php if ($randomPosts->have_posts()) : ?>
+    <?php while ($randomPosts->have_posts()) : $randomPosts->the_post(); ?>
+      <div class="random-column clearfix">
+        <?php the_post_thumbnail('small-thumbnail'); ?>
 
-    endwhile;
+        <div class="random-posts">
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <?php the_excerpt(); ?>
+        </div>
+      </div>
+    <?php endwhile; ?>
+  <?php endif; ?>
+  <?php wp_reset_postdata(); ?>
+</div>
 
-    else :
-      echo '<p>No content found</p>';
+<div class="one-half-column">
+  <!-- dev posts loop begins here -->
+  <?php $devPosts = new WP_Query('cat=12&posts_per_page=2'); ?>
 
-    endif; ?>
+    <?php if ($devPosts->have_posts()) : ?>
+      <?php while ($devPosts->have_posts()) : $devPosts->the_post(); ?>
 
-    
+<div class="random-column clearfix">
+  <?php the_post_thumbnail('small-thumbnail'); ?>
 
+  <div class="random-posts">
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_excerpt(); ?>
+  </div>
+</div>
+
+      <?php endwhile; ?>
+    <?php endif; ?>
+    <?php wp_reset_postdata(); ?>
+</div>
+
+ </div>
 </div>
 <!--site-content-->
 
 
-<?php get_footer();
-
- ?>
+<?php get_footer(); ?>
